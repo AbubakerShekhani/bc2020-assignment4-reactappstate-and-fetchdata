8fc46f55095306f149753c842073c589833f5b4c
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-06-10 09:37:37 +0500

    Create Room component (Assignment 4)

---
+++ src/Room.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function Room() {
+  const [isLit, setLit] = useState(false);
+  const brightness = isLit ? "lit" : "dark";
+  const [currentTemperature, setTemperature] = useState(72);
+
+  return (
+    <div className={`room ${brightness}`}>
+      <h1>Assignment #4 (React State - Lights ON/OFF) & </h1>
+      the room is {isLit ? "lit" : "dark"}
+      <br />
+      <button onClick={() => setLit(!isLit)}>Flip</button>
+      <button onClick={() => setLit(true)}>On</button>
+      <button onClick={() => setLit(false)}>Off</button>
+      <br />
+      {isLit ? (
+        <div>
+          <p>
+            Temperature: {currentTemperature} <span />
+            <button onClick={() => setTemperature(currentTemperature - 1)}>
+              -
+            </button>
+            <button onClick={() => setTemperature(currentTemperature + 1)}>
+              +
+            </button>
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
